98fa3074ba61b61ef08741e9a0b6eec879fb2e77
Mihhail Matvejev <m4tveevm@icloud.com>
2022-11-22 16:13:41 +0300

    Final release update

--- main.py
+++ main.py
@@ -4,25 +4,138 @@ import sqlite3
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+# Класс окна ввода логина и пароля
 class Login_UI(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('login.ui', self)
-        self.loginUI()
-        self.login = ''
+
+    def setupUi(self):
+        # загрузка ui
+
+        self.setObjectName("LoginWindow")
+        self.resize(300, 350)
+        self.setMaximumSize(QtCore.QSize(300, 350))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("img/logo.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
+        self.setTabShape(QtWidgets.QTabWidget.Rounded)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(20, 0, 261, 101))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.aircraft_logo = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.aircraft_logo.setText("")
+        self.aircraft_logo.setPixmap(QtGui.QPixmap("img/Delta-A350.png"))
+        self.aircraft_logo.setAlignment(QtCore.Qt.AlignCenter)
+        self.aircraft_logo.setObjectName("aircraft_logo")
+        self.verticalLayout_4.addWidget(self.aircraft_logo)
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(20, 110, 62, 46))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.Login = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.Login.setObjectName("Login")
+        self.verticalLayout_5.addWidget(self.Login)
+        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(80, 110, 201, 41))
+        self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
+        self.verticalLayout_6 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
+        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        self.login_input = QtWidgets.QLineEdit(self.verticalLayoutWidget_3)
+        self.login_input.setMaxLength(30)
+        self.login_input.setObjectName("login_input")
+        self.verticalLayout_6.addWidget(self.login_input)
+        self.verticalLayoutWidget_4 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(80, 155, 201, 51))
+        self.verticalLayoutWidget_4.setObjectName("verticalLayoutWidget_4")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_4)
+        self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        self.password_input = QtWidgets.QLineEdit(self.verticalLayoutWidget_4)
+        self.password_input.setMaxLength(30)
+        self.password_input.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password_input.setObjectName("password_input")
+        self.verticalLayout_7.addWidget(self.password_input)
+        self.verticalLayoutWidget_5 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_5.setGeometry(QtCore.QRect(20, 160, 62, 46))
+        self.verticalLayoutWidget_5.setObjectName("verticalLayoutWidget_5")
+        self.verticalLayout_8 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_5)
+        self.verticalLayout_8.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_8.setObjectName("verticalLayout_8")
+        self.Pass = QtWidgets.QLabel(self.verticalLayoutWidget_5)
+        self.Pass.setObjectName("Pass")
+        self.verticalLayout_8.addWidget(self.Pass)
+        self.verticalLayoutWidget_6 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_6.setGeometry(QtCore.QRect(20, 210, 261, 41))
+        self.verticalLayoutWidget_6.setObjectName("verticalLayoutWidget_6")
+        self.verticalLayout_9 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_6)
+        self.verticalLayout_9.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_9.setObjectName("verticalLayout_9")
+        self.error_box = QtWidgets.QLabel(self.verticalLayoutWidget_6)
+        self.error_box.setText("")
+        self.error_box.setObjectName("error_box")
+        self.verticalLayout_9.addWidget(self.error_box)
+        self.verticalLayoutWidget_7 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_7.setGeometry(QtCore.QRect(90, 260, 101, 31))
+        self.verticalLayoutWidget_7.setObjectName("verticalLayoutWidget_7")
+        self.verticalLayout_10 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_7)
+        self.verticalLayout_10.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_10.setObjectName("verticalLayout_10")
+        self.login_submit_btn = QtWidgets.QPushButton(self.verticalLayoutWidget_7)
+        self.login_submit_btn.setObjectName("login_submit_btn")
+        self.verticalLayout_10.addWidget(self.login_submit_btn)
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 300, 21))
+        self.menubar.setObjectName("menubar")
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+        self.actionMain = QtWidgets.QAction(self)
+        self.actionMain.setObjectName("actionMain")
+
+        self.retranslateUi(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self, LoginWindow):
+        _translate = QtCore.QCoreApplication.translate
+        LoginWindow.setWindowTitle(_translate("LoginWindow", "Авторизация"))
+        self.Login.setText(_translate("LoginWindow", "Login:"))
+        self.Pass.setText(_translate("LoginWindow", "Pass:"))
+        self.login_submit_btn.setText(_translate("LoginWindow", "Login"))
+        self.actionMain.setText(_translate("LoginWindow", "Main"))
 
     def connect_db(self, request):
-        con = sqlite3.connect('airport.db')
-        cur = con.cursor()
-        result = cur.execute(
-            f'''{request}''').fetchall()
-        con.close()
-        return result
+        # Функция, которая упрощает соединение с ДБ.
+        # А, как следствие, уменьшает количество требуемых строчек кода, для реализации
+        # запроссов к бд.
+
+        try:
+            con = sqlite3.connect('airport.db')
+            cur = con.cursor()
+            result = cur.execute(
+                f'''{request}''').fetchall()
+            con.close()
+            return result
+        except:
+            print('Произошла ошибка при подключении к БД. '
+                  'Проверьте имеется ли файл airport.db в папке с файлом. \n'
+                  'А так же проверьте не допустили ли вы ошибку в структуре БД.')
 
     def check_login(self):
+        # Функция, которая проверяет правильность ввода логина и пароля user'a в бд.
+        # А так же она проверяет имется ли введенный в программе логин
+        # или отсутствует в бд.
+
         password = self.password_input.text()
         login = self.login_input.text()
         result_login = self.connect_db(
@@ -32,11 +145,13 @@ class Login_UI(QMainWindow):
             and password = "{password}"''')
         if len(result_login) == 0:
             return 'login error'
-        if len(result_login) > 0:
+        elif result_login:
             self.login = login
             return result
 
-    def login(self):
+    def login_pass_checking(self):
+        # функция проверяющая логин и пароль по правилу.
+
         if len(self.login_input.text()) == 0:
             self.error_box.setText(
                 "Ошибка: Поле Логина(Login)\n не как не может быть пустым!")
@@ -63,8 +178,13 @@ class Login_UI(QMainWindow):
                 os.system(f'python main_screen.py {self.login}')
             self.password_input.setText('')
 
-    def loginUI(self):
-        self.login_submit_btn.clicked.connect(self.login)
+    def __init__(self):
+        # инициализация ui интерфейса.
+
+        super().__init__()
+        self.setupUi()
+        self.login = ''
+        self.login_submit_btn.clicked.connect(self.login_pass_checking)
 
 
 def main():
--- main_screen.py
+++ main_screen.py
@@ -1,20 +1,173 @@
+# --- <imports> ---
 import sqlite3
 import sys
 import requests
 from bs4 import BeautifulSoup
 import pymorphy2
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QHeaderView
+from PyQt5 import QtCore, QtWidgets
+
+# --- </imports> ---
+
+
+# Вывод метода анализа текста, для удобного использования в коде.
 
 morph = pymorphy2.MorphAnalyzer()
 
 
+# Класс основного окна
 class MainWindow(QMainWindow):
+    def setupUi(self):
+        # Основной экран
+
+        self.setObjectName("MainWindow")
+        self.resize(970, 540)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(580, 10, 381, 231))
+        self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.main_weather_header = QtWidgets.QLabel(self.verticalLayoutWidget_3)
+        self.main_weather_header.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.main_weather_header.setObjectName("main_weather_header")
+        self.verticalLayout_3.addWidget(self.main_weather_header)
+        self.main_weather_info = QtWidgets.QLabel(self.verticalLayoutWidget_3)
+        self.main_weather_info.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.main_weather_info.setObjectName("main_weather_info")
+        self.verticalLayout_3.addWidget(self.main_weather_info)
+        self.additional_weather = QtWidgets.QTextBrowser(self.verticalLayoutWidget_3)
+        self.additional_weather.setObjectName("additional_weather")
+        self.verticalLayout_3.addWidget(self.additional_weather)
+        self.verticalLayoutWidget_4 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(580, 250, 381, 231))
+        self.verticalLayoutWidget_4.setObjectName("verticalLayoutWidget_4")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_4)
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.destination_weather_header = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.destination_weather_header.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.destination_weather_header.setObjectName("destination_weather_header")
+        self.verticalLayout_4.addWidget(self.destination_weather_header)
+        self.destination_weather_info = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.destination_weather_info.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.destination_weather_info.setObjectName("destination_weather_info")
+        self.verticalLayout_4.addWidget(self.destination_weather_info)
+        self.destination_weather = QtWidgets.QTextBrowser(self.verticalLayoutWidget_4)
+        self.destination_weather.setObjectName("destination_weather")
+        self.verticalLayout_4.addWidget(self.destination_weather)
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 561, 31))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.welcome_title = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.welcome_title.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.welcome_title.setAlignment(
+            QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
+        self.welcome_title.setObjectName("welcome_title")
+        self.verticalLayout.addWidget(self.welcome_title)
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(0, 240, 571, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.line_2 = QtWidgets.QFrame(self.centralwidget)
+        self.line_2.setGeometry(QtCore.QRect(560, 0, 20, 511))
+        self.line_2.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 50, 461, 181))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.airline_flights = QtWidgets.QTableWidget(self.verticalLayoutWidget_2)
+        self.airline_flights.setObjectName("airline_flights")
+        self.airline_flights.setColumnCount(0)
+        self.airline_flights.setRowCount(0)
+        self.verticalLayout_2.addWidget(self.airline_flights)
+        self.verticalLayoutWidget_5 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_5.setGeometry(QtCore.QRect(10, 280, 461, 201))
+        self.verticalLayoutWidget_5.setObjectName("verticalLayoutWidget_5")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_5)
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.aircraft_information = QtWidgets.QTableWidget(self.verticalLayoutWidget_5)
+        self.aircraft_information.setObjectName("aircraft_information")
+        self.aircraft_information.setColumnCount(0)
+        self.aircraft_information.setRowCount(0)
+        self.verticalLayout_5.addWidget(self.aircraft_information)
+        self.verticalLayoutWidget_6 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_6.setGeometry(QtCore.QRect(480, 120, 81, 41))
+        self.verticalLayoutWidget_6.setObjectName("verticalLayoutWidget_6")
+        self.verticalLayout_6 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_6)
+        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        self.update_btn = QtWidgets.QPushButton(self.verticalLayoutWidget_6)
+        self.update_btn.setObjectName("update_btn")
+        self.verticalLayout_6.addWidget(self.update_btn)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 260, 281, 16))
+        self.label_2.setObjectName("label_2")
+        self.verticalLayoutWidget_7 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_7.setGeometry(QtCore.QRect(480, 280, 81, 121))
+        self.verticalLayoutWidget_7.setObjectName("verticalLayoutWidget_7")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_7)
+        self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_7.setSpacing(20)
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget_7)
+        self.label.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.label.setObjectName("label")
+        self.verticalLayout_7.addWidget(self.label)
+        self.search_input = QtWidgets.QLineEdit(self.verticalLayoutWidget_7)
+        self.search_input.setObjectName("search_input")
+        self.verticalLayout_7.addWidget(self.search_input)
+        self.search_btn = QtWidgets.QPushButton(self.verticalLayoutWidget_7)
+        self.search_btn.setObjectName("search_btn")
+        self.verticalLayout_7.addWidget(self.search_btn)
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 970, 21))
+        self.menubar.setObjectName("menubar")
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+
+        self.retranslateUi(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(
+            _translate("MainWindow", "UpdatingSpace / SmartTimeTable"))
+        self.main_weather_header.setText(_translate("MainWindow", "Погода Tallinn:"))
+        self.main_weather_info.setText(_translate("MainWindow", "Прогноз на сегодня:"))
+        self.destination_weather_header.setText(
+            _translate("MainWindow", "Погода в пункте назначения:"))
+        self.destination_weather_info.setText(
+            _translate("MainWindow", "Прогноз на сегодня:"))
+        self.welcome_title.setText(
+            _translate("MainWindow", "Добрый день, user. Вот список рейсов вашей авиа\n"
+                                     "компании:"))
+        self.update_btn.setText(_translate("MainWindow", "Обновить"))
+        self.label_2.setText(_translate("MainWindow", "Информация о самолёте:"))
+        self.label.setText(_translate("MainWindow", "Рейс:"))
+        self.search_btn.setText(_translate("MainWindow", "Искать"))
+
     def __init__(self):
+        # инициализация UI
+
         super().__init__()
-        uic.loadUi("main.ui", self)
+        self.setupUi()
         self.search_btn.clicked.connect(self.show_flight_information)
         self.update_btn.clicked.connect(self.find_flights)
         self.additional_weather.setText(self.get_weather())
@@ -24,31 +177,49 @@ class MainWindow(QMainWindow):
         self.find_flights()
 
     def connect_db(self, request):
-        con = sqlite3.connect('airport.db')
-        cur = con.cursor()
-        result = cur.execute(
-            f'''{request}''').fetchall()
-        con.close()
-        return result
+        # Функция, которая упрощает соединение с ДБ.
+        # А, как следствие, уменьшает количество требуемых строчек кода, для реализации
+        # запроссов к бд.
+
+        try:
+            con = sqlite3.connect('airport.db')
+            cur = con.cursor()
+            result = cur.execute(
+                f'''{request}''').fetchall()
+            con.close()
+            return result
+        except:
+            print('Произошла ошибка при подключении к БД. '
+                  'Проверьте имеется ли файл airport.db в папке с файлом. \n'
+                  'А так же проверьте не допустили ли вы ошибку в структуре БД.')
 
     def get_weather(self, city='Таллинн'):
-        url = f'https://sinoptik.ua/погода-{city.lower()}'
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
-        AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
-            'Accept-Language': 'en-US,en;q=0.9'}
-        response = requests.get(url, headers=headers)
-        bs = BeautifulSoup(response.text, "html.parser")
+        # Функция поиска погоды по параметру city.
+
+        try:
+            url = f'https://sinoptik.ua/погода-{city.lower()}'
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
+            AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+                'Accept-Language': 'en-US,en;q=0.9'}
+            response = requests.get(url, headers=headers)
+            bs = BeautifulSoup(response.text, "html.parser")
+        except:
+            return 'Ну удаётся подключится к сайту. Проверьте соединение с интернетом \n' \
+                   'Но а так же проверьте не заблокирован ли сайт https://sinoptik.ua/' \
+                   'в вашей стране.'
         try:
             temp = bs.find("p", {
                 "class": "today-temp"}).get_text(strip=True)
             description = bs.find("div", {
                 "class": "description"}).get_text(strip=True)
-            return f"Температура воздуха сейчас {temp}\n{description}"
         except:
-            print(f'Город "{city}" не найден.')
+            return f'Город "{city}" не найден. Либо отсутствует соединение с интернетом'
+        return f"Температура воздуха сейчас {temp}\n{description}"
 
     def find_flights(self):
+        # Функция нахождения рейсов авикомпании user'a.
+
         result = self.connect_db(
             f'''SELECT flight_id, destination, date, time_in_flight, airline FROM flights 
             where flights.airline = (select users.airline from users where login =
@@ -66,6 +237,7 @@ class MainWindow(QMainWindow):
         self.airline_flights.setHorizontalHeaderLabels(
             ["Номер рейса", "Пункт назначения", "Дата и время вылета", "Время в пути",
              "Авиакомпания"])
+
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.airline_flights.setItem(i, j, QTableWidgetItem(str(val)))
@@ -73,9 +245,15 @@ class MainWindow(QMainWindow):
             QHeaderView.ResizeToContents)
         self.airline_flights.horizontalHeader().setMinimumSectionSize(0)
 
+        # Обновление показаний погоды(на случай если до этого не было интернета)
+        self.additional_weather.setText(self.get_weather())
+
     def show_flight_information(self):
+        # функция выдающая информацию о самолёте из рейса.
+        # А так же показывающая погоду основываясь на destanation рейса.
+
         flight = self.search_input.text().upper()
-        if len(flight) > 0:
+        if flight:
             aircraft_id = self.connect_db(
                 f'''SELECT aircraft_id from flights where flight_id = "{flight}"''')
             if aircraft_id:
@@ -83,9 +261,9 @@ class MainWindow(QMainWindow):
                 SELECT destination from flights where flight_id = "{flight}"''')[0][0]
                 self.destination_weather_header.setText(f"Прогноз погоды в {destination}")
                 self.destination_weather.setText(self.get_weather(destination))
-                aircraft_db_info = self.connect_db(f'''SELECT model, date_of_manufacture, mileage,
-                speed, engines, wingspan, height, length FROM aircrafts where aircrafts.id
-                 = "{aircraft_id[0][0]}"''')
+                aircraft_db_info = self.connect_db(f'''SELECT model, date_of_manufacture,
+                mileage, speed, engines, wingspan, height, length FROM aircrafts where 
+                aircrafts.id = "{aircraft_id[0][0]}"''')
                 self.aircraft_information.setRowCount(len(aircraft_db_info))
                 self.statusBar().showMessage(f"Загружаю данные по рейсу {flight}")
                 self.aircraft_information.setColumnCount(len(aircraft_db_info[0]))
@@ -105,8 +283,15 @@ class MainWindow(QMainWindow):
         else:
             self.statusBar().showMessage("Поле поиска не может оставаться пустым!")
 
+        # Обновление показаний погоды(на случай если до этого не было интернета)
+        self.additional_weather.setText(self.get_weather())
+
 
 if __name__ == '__main__' and len(sys.argv) == 2:
+    # Запуск файла.
+    # А так же защита от самопроизвольного запуска пользователя
+    # (с его обходом авторизации через login.ui)
+
     login = sys.argv[1]
     app = QApplication(sys.argv)
     ex = MainWindow()
